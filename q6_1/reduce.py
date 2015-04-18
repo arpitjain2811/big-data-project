d48c65e2626243873af71a05dce0158f9437b07f
JiaminXuan <JiaminXuan@users.noreply.github.com>
2015-04-18 01:20:53 -0400

    Handling Exceptions

--- q6_1/reduce.py
+++ q6_1/reduce.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import sys, os
+import sys,os
 from datetime import datetime
 current_driver = None
 current_day=None
@@ -11,20 +11,24 @@ for line in sys.stdin:
 		time_list[1]=time
 	else:
 		if current_day and current_driver:
-			minhour=datetime.strptime(time_list[0],'%H:%M:%S')
-			maxhour=datetime.strptime(time_list[1],'%H:%M:%S')
-			hours=maxhour-minhour
-			hours=hours.total_seconds()
-			hours=hours/3600
-			print "%s,%s,%.2f"%(driver,day,hours)
+			try:
+				minhour=datetime.strptime(time_list[0],'%H:%M:%S')
+				maxhour=datetime.strptime(time_list[1],'%H:%M:%S')
+				hours=maxhour-minhour
+				hours=hours.total_seconds()
+				hours=hours/3600
+				print "%s,%s,%.2f"%(driver,day,hours)
+			else:
+				pass
 		current_driver=driver
 		current_day=day
 		time_list[0]=time
-if current_day and current_driver:
+try:
 	minhour=datetime.strptime(time_list[0],'%H:%M:%S')
 	maxhour=datetime.strptime(time_list[1],'%H:%M:%S')
 	hours=maxhour-minhour
 	hours=hours.total_seconds()
 	hours=hours/3600
 	print "%s,%s,%.2f"%(driver,day,hours)
-
+else:
+	pass
