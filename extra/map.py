be9c76fcee7e62f852e256a54ddc06237549b3f3
Arpit Jain <arpit.jain2811@gmail.com>
2015-05-02 04:04:49 -0400

    testing

---
+++ extra/map.py
@@ -0,0 +1,52 @@
+# Trip Table
+# medallion             0
+# hack_license          1
+# vendor_id             2
+# rate_code             3
+# store_and_fwd_flag    4
+# pickup_datetime       5
+# dropoff_datetime      6
+# passenger_count       7
+# trip_time_in_secs     8
+# trip_distance         9
+# pickup_longitude      10
+# pickup_latitude       11
+# dropoff_longitude     12
+# dropoff_latitude      13
+
+# Fare Table
+# medallion             0
+# hack_license          1
+# vendor_id             2
+# pickup_datetime       3
+# payment_type          4
+# fare_amount           5
+# surcharge             6
+# mta_tax               7
+# tip_amount            8
+# tolls_amount          9
+# total_amount          10
+
+#!/usr/bin/python
+import sys
+import StringIO
+import csv
+
+# input comes from STDIN (stream data that goes to the program)
+for line in sys.stdin:
+
+    csv_file = StringIO.StringIO(line)
+    csv_reader = csv.reader(csv_file)
+
+    for l in csv_reader:
+
+        #l = line.strip().split(',')
+        # Data from vehicle table
+
+        if len(l) == 11:
+            #tag = "fare"
+            if l[0] == "medallion":
+                attributes_fare = l
+                continue
+            else:
+                print("%s\t%f\t%d" % (l[1],float(l[10]),1))
---
+++ extra/reduce.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import sys
+
+current_hack = None
+current_num_trips = 0
+current_total = 0.0
+
+
+# input comes from STDIN (stream data that goes to the program)
+for line in sys.stdin:
+
+    hack, total , count = line.strip().split("\t")
+
+    try:
+        total = float(total)
+        count = int(count)
+    except ValueError:
+        continue
+
+    if hack == current_hack:
+        current_total += total
+        current_num_trips += count
+    else:
+        if current_hack:
+            # output goes to STDOUT (stream data that the program writes)
+            print("%s\t%f" %( current_hack, current_total/current_num_trips ))
+        current_hack = hack
+        current_num_trips = count
+        current_total = total
+
+if current_hack == hack:
+    print('%s\t%f' % (current_hack, current_total/current_num_trips))
