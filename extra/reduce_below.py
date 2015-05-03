15dc5f26ba1a243c36623ffac8e36d9f81683060
Arpit Jain <arpit.jain2811@gmail.com>
2015-05-02 22:44:20 -0400

    typo

--- extra/reduce_below.py
+++ extra/reduce_below.py
@@ -4,14 +4,14 @@ import sys
 
 current_date = None
 current_total_amount = 0.0
-current_num_trips = 0.0
+current_num_trips = 0
 # input comes from STDIN (stream data that goes to the program)
 for line in sys.stdin:
     date, total_amount, num_trips = line.strip().split("\t")
-
+    print date
     try:
         total_amount = float(total_amount)
-        num_trips = float(num_trips)
+        num_trips = int(num_trips)
     except ValueError:
         continue
 
@@ -21,10 +21,10 @@ for line in sys.stdin:
     else:
         if current_date:
             # output goes to STDOUT (stream data that the program writes)
-            print("%s,%.2f,%.2f,%.2f,%.2f" %( current_date, current_total_amount,current_num_trips ))
+            print("%s,%.2f,%d" %( current_date, current_total_amount,current_num_trips ))
         current_date = date
         current_total_amount = total_amount
         current_num_trips = num_trips
 
 if current_date == date:
-    print("%s,%.2f,%.2f,%.2f,%.2f" %( current_date, current_total_amount,current_num_trips ))
+    print("%s,%.2f,%d" %( current_date, current_total_amount,current_num_trips ))
