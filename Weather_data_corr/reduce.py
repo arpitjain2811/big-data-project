11346547714d47e7d7541bd723f80d3e657c477f
Arpit Jain <arpit.jain2811@gmail.com>
2015-05-04 23:40:54 -0400

    weather vendor

---
+++ Weather_data_corr/reduce.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import sys
+
+current_date = None
+current_total_amount = 0.0
+current_num_trips = 0
+# input comes from STDIN (stream data that goes to the program)
+for line in sys.stdin:
+    date, total_amount, num_trips = line.strip().split("\t")
+    try:
+        total_amount = float(total_amount)
+        num_trips = int(num_trips)
+    except ValueError:
+        continue
+
+    if date == current_date:
+        current_total_amount += total_amount
+        current_num_trips += num_trips
+    else:
+        if current_date:
+            # output goes to STDOUT (stream data that the program writes)
+            print("%s\t%.2f\t%d" %( current_date, current_total_amount,current_num_trips ))
+        current_date = date
+        current_total_amount = total_amount
+        current_num_trips = num_trips
+
+if current_date == date:
+    print("%s\t%.2f\t%d" %( current_date, current_total_amount,current_num_trips ))
