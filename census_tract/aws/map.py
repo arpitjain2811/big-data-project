f09c6293b31438bfcde43fca4adb057d0c4f27fa
Arpit Jain <arpit.jain2811@gmail.com>
2015-05-07 23:36:04 -0400

    new map_loc

--- census_tract/aws/map.py
+++ census_tract/aws/map.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import sys
 import csv
+import StringIO
 from shapely.geometry import Point
 import geopandas as gpd
 myShapefile = gpd.read_file('tract.shp')
@@ -11,17 +12,19 @@ nyc_tract=myShapefile[myShapefile.COUNTYFP.isin(countylist)]
 
 # input comes from STDIN (stream data that goes to the program)
 for line in sys.stdin:
-    csv_reader = line.strip().split(',')
-    for line in csv_reader:
-        if line[0] != "medallion":
+
+    csv_file = StringIO.StringIO(line)
+    csv_reader = csv.reader(csv_file)
+    for l in csv_reader:
+        if l[0] != "medallion":
             try:
-                pickup_point=Point(float(line[10]),float(line[11]))
-                line.append(str(nyc_tract.TRACTCE[nyc_tract.geometry.contains(pickup_point)].tolist()[0]))
+                pickup_point=Point(float(l[10]),float(l[11]))
+                l.append(str(nyc_tract.TRACTCE[nyc_tract.geometry.contains(pickup_point)].tolist()[0]))
             except:
-                line.append('<o>')
+                l.append('<o>')
             try:
-                dropoff_point=Point(float(line[12]),float(line[13]))
-                line.append(str(nyc_tract.TRACTCE[nyc_tract.geometry.contains(dropoff_point)].tolist()[0]))
+                dropoff_point=Point(float(l[12]),float(l[13]))
+                l.append(str(nyc_tract.TRACTCE[nyc_tract.geometry.contains(dropoff_point)].tolist()[0]))
             except:
-                line.append('<o>')
-            print ','.join(line)
+                l.append('<o>')
+            print ','.join(l)
--- census_tract/aws/setup.sh
+++ census_tract/aws/setup.sh
@@ -1,9 +1,4 @@
-#!/bin/bash 
-sudo yum -y install git gcc python-dev python-devel
-sudo pip install -U numpy 
-sudo pip install pyyaml nltk 
-sudo pip install -e git://github.com/mdp-toolkit/mdp-toolkit#egg=MDP 
-sudo pip install geopandas
 sudo yum-config-manager --enable epel
 sudo yum install -y spatialindex spatialindex-devel
 sudo ln -sf /usr/bin/python2.7 /usr/bin/python
+sudo pip install Rtree==0.7.0
