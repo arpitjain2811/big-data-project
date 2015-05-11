19e76662ec8fb8f8550a047f1272530cc3faadfc
Arpit Jain <arpit.jain2811@gmail.com>
2015-05-10 21:36:05 -0400

    Removed

--- q5/reduce.py
+++ q5/reduce.py
@@ -7,7 +7,7 @@ dropoff=None
 dis=.0
 count=0
 for line in sys.stdin:
-	driver,pickup_neighbor,dropoff_neighbor,trip_dis=line.strip('\t').split(',')
+	pickup_neighbor,dropoff_neighbor,trip_dis=line.strip('\t').split(',')
 	trip_dis=float(trip_dis)
 	# print trip_dis,count
 	if pickup==pickup_neighbor and dropoff==dropoff_neighbor:
