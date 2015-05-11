38350eaaae8e9e0e3520846a0baf4c0861d9cb94
Arpit Jain <arpit.jain2811@gmail.com>
2015-05-10 21:06:58 -0400

    testing q4

--- census_tract/aws/map.py
+++ census_tract/aws/map.py
@@ -27,7 +27,7 @@ def readNeighborhood(shapeFilename, index, neighborhoods):
 
 def parseInput():
     for line in sys.stdin:
-        line = line.strip('\n')
+        line = line.strip()
         values = line.split(',')
         if len(values)>1 and values[0]!='medallion': 
             yield values
@@ -39,25 +39,30 @@ def mapper():
     agg = {}
     for values in parseInput():
         try:
-            pickup_location = (float(values[10]), float(values[11]))
-            dropoff_location = (float(values[12]), float(values[13]))
-            dropoff_neighborhood = findNeighborhood(dropoff_location, index, neighborhoods)
-            pickup_neighborhood = findNeighborhood(pickup_location, index, neighborhoods)
-            if pickup_neighborhood!=-1:
-                pickup_name = neighborhoods[pickup_neighborhood][0]
-            else:
-                pickup_name = "UNKNOWN"
-            if dropoff_neighborhood!=-1:
-                dropoff_name = neighborhoods[dropoff_neighborhood][0]
-            else:
-                dropoff_name = "UNKNOWN"    
-            values.append(str(pickup_name))
-            values.append(str(dropoff_name))
-            print ','.join(values)
-        except:
-            values.append("UNKNOWN")
-            values.append("UNKNOWN")
-            print ','.join(values)
+            try:
+                p1 = float(values[10])
+                p2 = float(values[11])
+                d1 = float(values[12])
+                d2 = float(values[13])
+            except ValueError:
+                continue;
+                pickup_location = (p1, p2)
+                dropoff_location = (d1, d2)
+                dropoff_neighborhood = findNeighborhood(dropoff_location, index, neighborhoods)
+                pickup_neighborhood = findNeighborhood(pickup_location, index, neighborhoods)
+                if pickup_neighborhood!=-1:
+                    pickup_name = neighborhoods[pickup_neighborhood][0]
+                else:
+                    pickup_name = "UNKNOWN"
+                if dropoff_neighborhood!=-1:
+                    dropoff_name = neighborhoods[dropoff_neighborhood][0]
+                else:
+                    dropoff_name = "UNKNOWN"    
+                values.append(str(pickup_name))
+                values.append(str(dropoff_name))
+                print ','.join(values)
+        except :
+            continue;
     
 if __name__=='__main__':
     mapper()
--- q4/map.py
+++ q4/map.py
@@ -1,12 +1,48 @@
 #!/usr/bin/python
+# Trip Table
+# medallion             0
+# hack_license          1
+# vendor_id             2
+# rate_code             3
+# store_and_fwd_flag    4
+# pickup_datetime       5
+# dropoff_datetime      6
+# passenger_count       7
+# trip_time_in_secs     8
+# trip_distance         9
+# pickup_longitude      10
+# pickup_latitude       11
+# dropoff_longitude     12
+# dropoff_latitude      13
+
+# Fare Table
+# medallion             0
+# hack_license          1
+# vendor_id             2
+# pickup_datetime       3
+# payment_type          4
+# fare_amount           5
+# surcharge             6
+# mta_tax               7
+# tip_amount            8
+# tolls_amount          9
+# total_amount          10
+
+
 import sys
 import StringIO
 import csv
+import datetime
+
+def get_key(date):
+    return date
 
 # input comes from STDIN (stream data that goes to the program)
+H = {}
+N = {}
 for line in sys.stdin:
 
-    csv_file = StringIO.StringIO(line)
+    csv_file = StringIO.StringIO(line.strip())
     csv_reader = csv.reader(csv_file)
 
     for l in csv_reader:
@@ -14,10 +50,19 @@ for line in sys.stdin:
         #l = line.strip().split(',')
         # Data from vehicle table
 
-        if len(l) == 14:
+        if len(l) > 0:
             #tag = "fare"
             if l[0] == "medallion":
-                attributes_trip = l
+                attributes_fare = l
                 continue
             else:
-                print("%f\t%f\t%d" % (round(float(l[10]),3),round(float(l[11]),3),1))
+                try:
+                    pickup = l[14]
+                    dropoff = l[15]
+                    key = pickup + '|' + dropoff
+                    H[key] = H.get(key,0.0) + float(l[9])
+                    N[key] = N.get(key,0.0) + 1
+                except:
+                    continue;    
+for i in H.keys():
+    print("%s\t%f\t%d"%(i,H[i],N[i]))
